e42a3e871c0275b6adf1e8dbf5efd91cc6a2bbd2
Alexey Dmitriev <denshion@mail.ru>
2021-07-16 15:15:55 +0300

    Ported IR reading and PWM to ATTiny13. It does not work, though.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,10 @@
-#define F_CPU 16000000UL
-#define BAUD 9600
+#if defined (__AVR_ATmega328p__)
+	#define F_CPU 16000000UL
+#elif defined (__AVR_ATtiny13__)
+	#define F_CPU 1200000UL
+#else
+	#error "Device not supported"
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,44 +12,58 @@
 #include <avr/interrupt.h>
 #include <avr/sfr_defs.h>
 #include <avr/sfr_defs.h>
-
 #include <util/delay.h>
 
 #define PERIOD_MEASUREMENT_SOFTWARE
 #define HWTIMER_FREQUENCY 1777l
 #define PERIOD_TIMER_FREQUENCY 1777l
 #include "lib/timer_period.c"
-
 #include "lib/ir_nec.c"
 
 
-#include "uart.h"
-#include "uart.c"
-
-
-#define PORT_IR PINB
-#define PIN_IR 3
-
-#if __AVR_DEVICE_NAME__ != atmega328
-#error "Devices other than atmega328p not supported"
+#if defined (__AVR_ATmega328p__)
+	#define PIN_IR PINB
+	#define BIT_IR 3
+	#define PORT_PWM PORTB
+	#define DDR_PWM DDRB
+	#define BIT_PWM 1
+	#define F_CPU_DIV_1777Hz 140
+#elif defined (__AVR_ATtiny13__)
+	#define PIN_IR PINB
+	#define BIT_IR 0
+	#define PORT_PWM PORTB
+	#define DDR_PWM DDRB
+	#define BIT_PWM 2
+	#define F_CPU_DIV_1777Hz 84
 #endif
-
 
 ISR(PCINT0_vect){
 	/* Logical pin change interrupt vector. */
 	sei(); // Enable nested interrupts
-	if (((PORT_IR >> PIN_IR) & 1) == 0){
+	if (((PIN_IR >> BIT_IR) & 1) == 0){
 		ir_nec_process_pin_change();
 	}
 }
 
-
-uint8_t supertimer = 0xFF;
+volatile uint8_t mute = 0;
 
 inline void tick();
 
+// For some reason, these interrupts are named differently for ATMega and ATTiny
+#if defined (__AVR_ATtiny13__)
+	#define TIMER0_COMPA_vect TIM0_COMPA_vect
+	#define TIMER0_COMPB_vect TIM0_COMPB_vect
+#endif
+
+
+ISR(TIMER0_COMPB_vect){
+	PORT_PWM &= ~(1 << BIT_PWM);
+}
 ISR(TIMER0_COMPA_vect){
 	tick();
+	if (!mute){
+		PORT_PWM |= (1 << BIT_PWM);
+	}
 /* Timer reached value COMPA interrupt vector. */
 /* The timer generates a TIMER_FREQ Hz internal signal, which carries two functions:
  * 1. PWM for the motor;
@@ -54,56 +73,60 @@ ISR(TIMER0_COMPA_vect){
 }
 
 inline void tick(){
-
 	software_timer_tick((uint16_t) TCNT0);
-//	supertimer++;
-//	if (supertimer % MULTIPLE_1777Hz == 0) {
-//		ir_nec_tick();
-//	}
 }
 
 static inline void init_pendulum(){
-//	TCCR0A = _BV(WGM01) | _BV(COM0B1); // CTC; Set OC0B on Compare Match
-	TCCR0A = _BV(WGM01) | _BV(WGM00) | _BV(COM0B1); // CTC; Set OC0B on Compare Match
+#if defined (__AVR_ATmega328p__)
+	TCCR0A = _BV(WGM01) | _BV(WGM00); // CTC;
 	TCCR0B = _BV(WGM02) | _BV(CS00) | _BV(CS01); // divide system clock by 64
+#elif __AVR_DEVICE_NAME == attiny13
+	TCCR0A = _BV(WGM01) | _BV(WGM00); // CTC; 
+	TCCR0B = _BV(WGM02) | _BV(CS00) | _BV(CS01); // divide system clock by 64
+#endif
 
 	// Set up PWM by OCR0B compare
 
 	// Set Clear Timer on Compare Match (CTC) mode. In this mode, it acts as
 	// the oscillator (16 MHz) frequency divider, dividing it by the (OCR0 + 1) value.
-	OCR0A = 140; // 16 MHz / 223 = 2 x 36 kHz 
+	OCR0A = F_CPU_DIV_1777Hz; // 16 MHz / 223 = 2 x 36 kHz 
 	OCR0B = 0; // 16 MHz / 223 = 2 x 36 kHz
 
-	TIMSK0 = _BV(OCIE0A); // Enable interrupt
+	TIMSK0 = _BV(OCIE0A) | _BV(OCIE0B); // Enable interrupt
 }
 
 #define BEHOLDTV_REMOTECONTROL_ADDRESS 0x61D6
 #define HYUNDAI_REMOTECONTROL_ADDRESS 0x8E40
 
 int main(){
-	FILE ir_nec_file = fdev_open_ir_nec(ir_nec_in, IR_NEC_REPEAT_CODES_RESPECT, BEHOLDTV_REMOTECONTROL_ADDRESS, IR_NEC_ADDRESSMODE_EXACT);
-	FILE * ir_nec_in = &ir_nec_file;
+//	FILE ir_nec_file = fdev_open_ir_nec(ir_nec_in, IR_NEC_REPEAT_CODES_RESPECT, BEHOLDTV_REMOTECONTROL_ADDRESS, IR_NEC_ADDRESSMODE_EXACT);
+//	FILE * ir_nec_in = &ir_nec_file;
 
-	DDRD = (1 << 5); // Enable PWM out
-	DDRB = (1 << 5) | (1 << 4);
+#if defined (__AVR_ATmega328p__)
 	PCICR = 1 << PCIE0;
-	PCMSK0 = 1 << PIN_IR;
+	PCMSK0 = 1 << BIT_IR;
+#elif __AVR_DEVICE_NAME == attiny13
+	GIMSK = 1 << PCIE;
+	PCMSK = 1 << BIT_IR;
+#endif
+
+	DDR_PWM |= 1 << BIT_PWM;
+	DDRB |= 1 << 1;
 
 	sei();
 	
-	uart_init(_BV(TXEN0));
-	stdout = &uart_out;
-	printf("Hello!\n");
 	init_pendulum();
 	while (1){
 		_delay_ms(50);
-		uint8_t command = (uint8_t) ir_nec_getchar(ir_nec_in);
-		printf("Received command %d: %x\r\n", command, (uint8_t) command);
-		int8_t pwm = 0;
+		PORTB ^= (1 << 1);
+		//uint8_t command = (uint8_t) ir_nec_getchar(ir_nec_in);
+		uint8_t command = 123;
+		int8_t pwm = 9;
 		switch(command){
 		case 0:
-			pwm = 0;
-			break;
+			OCR0B = 0;
+			mute = 1;
+			continue;
 		case 0x80:
 			pwm = 1;
 			break;
@@ -134,8 +157,8 @@ int main(){
 		default:
 			continue;
 		}
-		OCR0B = 14 * pwm; // PWM
-		printf("Set OCR0B: %d/140\r\n", OCR0B);
+		mute = 0;
+		OCR0B = (F_CPU_DIV_1777Hz / 10) * pwm; // PWM
 	}
 	return 0;
 }
