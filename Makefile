481c9252cc397a956607863b60c91337f6160b74
Alexey Dmitriev <denshion@mail.ru>
2021-09-04 04:03:55 +0300

    Rewritten the IR NEC protocol completely. Added decoding a digit command that resolves to PWM power.

---
+++ Makefile
@@ -0,0 +1,39 @@
+MCU=attiny13
+CC=avr-gcc -mmcu=$(MCU)  -O2
+AS=avr-as -mmcu=$(MCU)
+#OBJCOPY=avr-objcopy -j .text -j .data -O ihex
+OBJCOPY=avr-objcopy -O ihex
+# TTY=/dev/ttyUSB0
+TTY=COM3
+DUDE=avrdude
+# DUDE=sudo avrdude
+
+all: build/main.hex
+
+build:
+	mkdir -p build
+
+build/main.hex: build/main.o
+	$(OBJCOPY) $< $@
+
+build/main.o: src/main.c build
+	$(CC) $< -o $@
+
+.PHONY: install eeprom clean
+
+# For some reason, this command is prone to yielding "Unexpected Device
+# signature, 0x0000" and "Verification error, first mismatch at byte 0x0000"
+# errors.  Just run it multiple times until succeeded. If it does not, try
+# moving the cables.
+install: build/main.hex
+	$(DUDE) -c avrisp -p $(MCU) -P $(TTY) -b 19200 -U flash:w:$<:i -F
+	$(DUDE) -c avrisp -p $(MCU) -P $(TTY) -b 19200 -U flash:w:$<:i
+
+# Read EEPROM memory into a file
+eeprom:
+	$(DUDE) -c avrisp -p $(MCU) -P $(TTY) -b 19200 -U eeprom:r:eeprom.hex:i
+
+clean:
+	rm -rf build
+	rm -f eeprom.hex
+
--- src/main.c
+++ src/main.c
@@ -1,164 +1,173 @@
-#if defined (__AVR_ATmega328p__)
-	#define F_CPU 16000000UL
-#elif defined (__AVR_ATtiny13__)
-	#define F_CPU 1200000UL
-#else
-	#error "Device not supported"
-#endif
+// A simple variable-PWM robot on ATTiny13
+#define F_CPU 1200000UL
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <avr/interrupt.h>
 #include <avr/sfr_defs.h>
-#include <avr/sfr_defs.h>
+#include <avr/eeprom.h>
 #include <util/delay.h>
 
-#define PERIOD_MEASUREMENT_SOFTWARE
-#define HWTIMER_FREQUENCY 1777l
-#define PERIOD_TIMER_FREQUENCY 1777l
-#include "lib/timer_period.c"
-#include "lib/ir_nec.c"
+#define BIT_IR 3
+#define BIT_PWM 1
+#define BIT_LED 2
 
+volatile uint8_t ir_receiving_bits_flag = 0;
+volatile uint8_t prev_TCNT0 = 0;
+volatile uint8_t ir_received_bits_count = 0;
+volatile uint32_t ir_shift_register = 0;
 
-#if defined (__AVR_ATmega328p__)
-	#define PIN_IR PINB
-	#define BIT_IR 3
-	#define PORT_PWM PORTB
-	#define DDR_PWM DDRB
-	#define BIT_PWM 1
-	#define F_CPU_DIV_1777Hz 140
-#elif defined (__AVR_ATtiny13__)
-	#define PIN_IR PINB
-	#define BIT_IR 0
-	#define PORT_PWM PORTB
-	#define DDR_PWM DDRB
-	#define BIT_PWM 2
-	#define F_CPU_DIV_1777Hz 84
-#endif
-
-ISR(PCINT0_vect){
-	/* Logical pin change interrupt vector. */
-	sei(); // Enable nested interrupts
-	if (((PIN_IR >> BIT_IR) & 1) == 0){
-		ir_nec_process_pin_change();
-	}
-}
-
-volatile uint8_t mute = 0;
-
-inline void tick();
-
-// For some reason, these interrupts are named differently for ATMega and ATTiny
-#if defined (__AVR_ATtiny13__)
-	#define TIMER0_COMPA_vect TIM0_COMPA_vect
-	#define TIMER0_COMPB_vect TIM0_COMPB_vect
-#endif
-
-
-ISR(TIMER0_COMPB_vect){
-	PORT_PWM &= ~(1 << BIT_PWM);
-}
-ISR(TIMER0_COMPA_vect){
-	tick();
-	if (!mute){
-		PORT_PWM |= (1 << BIT_PWM);
-	}
-/* Timer reached value COMPA interrupt vector. */
-/* The timer generates a TIMER_FREQ Hz internal signal, which carries two functions:
- * 1. PWM for the motor;
- * 2. At the end of each period, i.e., COMPA value reached by the timer, this interrupt vector is executed,
- * which triggers all actions: secondary counter increments and IR readout, ADC readout, etc.
- */
-}
-
-inline void tick(){
-	software_timer_tick((uint16_t) TCNT0);
-}
-
-static inline void init_pendulum(){
-#if defined (__AVR_ATmega328p__)
-	TCCR0A = _BV(WGM01) | _BV(WGM00); // CTC;
-	TCCR0B = _BV(WGM02) | _BV(CS00) | _BV(CS01); // divide system clock by 64
-#elif __AVR_DEVICE_NAME == attiny13
-	TCCR0A = _BV(WGM01) | _BV(WGM00); // CTC; 
-	TCCR0B = _BV(WGM02) | _BV(CS00) | _BV(CS01); // divide system clock by 64
-#endif
-
-	// Set up PWM by OCR0B compare
-
-	// Set Clear Timer on Compare Match (CTC) mode. In this mode, it acts as
-	// the oscillator (16 MHz) frequency divider, dividing it by the (OCR0 + 1) value.
-	OCR0A = F_CPU_DIV_1777Hz; // 16 MHz / 223 = 2 x 36 kHz 
-	OCR0B = 0; // 16 MHz / 223 = 2 x 36 kHz
-
-	TIMSK0 = _BV(OCIE0A) | _BV(OCIE0B); // Enable interrupt
-}
-
+// The addresses that the remote controls send
 #define BEHOLDTV_REMOTECONTROL_ADDRESS 0x61D6
 #define HYUNDAI_REMOTECONTROL_ADDRESS 0x8E40
 
+
+/* Logical pin change interrupt vector. */
+ISR(PCINT0_vect){
+	if((PINB >> BIT_IR) & 1){
+		// Only trigger on falling edge
+		return;
+	}
+
+	// Read the 8-bit Timer/Counter and calculate the time difference with
+	// the previous falling edge. This allows measuring periods from 54 us
+	// to 14 ms.
+	uint8_t period = TCNT0 - prev_TCNT0;
+	prev_TCNT0 = TCNT0;
+	
+	// The NEC code consists of a 9000 us negative + 4500 us positive = leading pulse,
+	// and 32 pulse-period-modulated bits which can be either
+	// 560 us pos. + 1680 us neg. for logical 1, or
+	// 560 us pos. + 560 us neg. for logical 0.
+	// An IR remote control also sends repeat codes if the key is held
+	// pressed, but we ignore them here.
+	if(period > 240){
+		// 9000 us + 4500 us = leading pulse (approx. 250 clock cycles)
+		// Start receiving bits.
+		// Clear the shift register and set the flag.
+		ir_receiving_bits_flag = 1;
+		ir_received_bits_count = 0;
+		ir_shift_register = 0;
+		// Turn on LED to show that the code is received now.
+		PORTB |= _BV(BIT_LED);
+	} else if(ir_receiving_bits_flag){
+		if(period > 15 && period < 25){
+			// 560 us + 560 us (approx. 20 clock cycles) = logical 0
+			ir_shift_register <<= 1;
+			ir_received_bits_count++;
+		} else if (period > 35 && period < 45){
+			// 560 us + 1680 us (approx. 40 clock cycles) = logical 1
+			ir_shift_register <<= 1;
+			ir_shift_register |= 1;
+			ir_received_bits_count++;
+		} else {
+			// If received anything else, this is an error, stop receiving bits.
+			ir_receiving_bits_flag = 0;
+		}
+
+		// All 32 bits have successfully been received.
+		if(ir_received_bits_count == 32){
+			ir_receiving_bits_flag = 0;
+
+			// Turn off LED to show that the code has been received
+			PORTB &= ~_BV(BIT_LED);
+
+			// Process received code
+			uint8_t command = (uint8_t) (ir_shift_register >> 8);
+
+//			// These checks don't work anyway, so we skip them.
+//
+//			// Remote control device selectivity
+//			if ((ir_shift_register >> 16) != BEHOLDTV_REMOTECONTROL_ADDRESS){
+//				// If this code is not from our remote control, do nothing
+//				return;
+//			}
+//			// Correct command verification
+//			if (command != ~((uint8_t) ir_shift_register)){
+//				// Command does not match its logical inverse which is
+//				// sent after it, so this is a malformed command.
+//				// Ignore it silently.
+//				return;
+//			}
+			
+			
+			switch(command){
+			// Button POWER
+			case 0x48:
+				OCR0B = 0; // 0% PWM
+				break;
+			// Button 1
+			case 0x80:
+				OCR0B = 26; // 10% PWM
+				break;
+			// Button 2
+			case 0x40:
+				OCR0B = 51; // 20% PWM
+				break;
+			// Button 3
+			case 0xc0:
+				OCR0B = 77; // 30% PWM
+				break;
+			// Button 4
+			case 0x20:
+				OCR0B = 102; // 40% PWM
+				break;
+			// Button 5
+			case 0xa0:
+				OCR0B = 127; // 50% PWM
+				break;
+			// Button 6
+			case 0x60:
+				OCR0B = 153; // 60% PWM
+				break;
+			// Button 7
+			case 0xe0:
+				OCR0B = 179; // 70% PWM
+				break;
+			// Button 8
+			case 0x10:
+				OCR0B = 204; // 80% PWM
+				break;
+			// Button 9
+			case 0x90:
+				OCR0B = 230; // 90% PWM
+				break;
+			// Button 0
+			case 0x0:
+				OCR0B = 255; // 100% PWM
+				break;
+			}
+		}
+	}
+	// Ignore any other pulses
+}
+
 int main(){
-//	FILE ir_nec_file = fdev_open_ir_nec(ir_nec_in, IR_NEC_REPEAT_CODES_RESPECT, BEHOLDTV_REMOTECONTROL_ADDRESS, IR_NEC_ADDRESSMODE_EXACT);
-//	FILE * ir_nec_in = &ir_nec_file;
-
-#if defined (__AVR_ATmega328p__)
-	PCICR = 1 << PCIE0;
-	PCMSK0 = 1 << BIT_IR;
-#elif __AVR_DEVICE_NAME == attiny13
+	// Enable Pin Change Interrupt
 	GIMSK = 1 << PCIE;
+	// Select only pin BIT_IR for Pin Change Interrupt
 	PCMSK = 1 << BIT_IR;
-#endif
 
-	DDR_PWM |= 1 << BIT_PWM;
-	DDRB |= 1 << 1;
+	// Open PWM and LED bits for writing
+	DDRB |= 1 << BIT_PWM;
+	DDRB |= 1 << BIT_LED;
 
+	// Global Enable interrupts
 	sei();
 	
-	init_pendulum();
+	// Init Timer/Counter for PWM generation and IR pulse decoding.
+	// Set Fast PWM mode with generation of a Non-inverting signal on pin OC0B.
+	// The 8-bit clock counts from 0 to 255 and starts again at zero. When
+	// it encounters the value OCR0B, it clears the OC0B bit, and sets it
+	// high again when the counter is restarted from zero.
+	TCCR0A = _BV(WGM01) | _BV(WGM00) | _BV(COM0B1);
+	// divide system clock by 64 (this gives 18.34 kHz), approx. 54 us per tick.
+	TCCR0B = 3;
+	
+//	uint8_t test[4] = {0xca, 0xfe, 0xba, 0xbe};
+//	eeprom_write_block(test, 0, 4);		
 	while (1){
-		_delay_ms(50);
-		PORTB ^= (1 << 1);
-		//uint8_t command = (uint8_t) ir_nec_getchar(ir_nec_in);
-		uint8_t command = 123;
-		int8_t pwm = 9;
-		switch(command){
-		case 0:
-			OCR0B = 0;
-			mute = 1;
-			continue;
-		case 0x80:
-			pwm = 1;
-			break;
-		case 0x40:
-			pwm = 2;
-			break;
-		case 0xc0:
-			pwm = 3;
-			break;
-		case 0x20:
-			pwm = 4;
-			break;
-		case 0xa0:
-			pwm = 5;
-			break;
-		case 0x60:
-			pwm = 6;
-			break;
-		case 0xe0:
-			pwm = 7;
-			break;
-		case 0x10:
-			pwm = 8;
-			break;
-		case 0x90:
-			pwm = 9;
-			break;
-		default:
-			continue;
-		}
-		mute = 0;
-		OCR0B = (F_CPU_DIV_1777Hz / 10) * pwm; // PWM
+		_delay_ms(250);
 	}
 	return 0;
 }
